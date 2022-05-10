237c6d056c0bd71631b933a6b2f8063e4afa1f04
Santiago Di Fiore <difiore.san@gmail.com>
2022-05-10 12:24:18 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Barchart from './Chart'
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Barchart />
+    </div>
+  )
+}
+
+export default App
